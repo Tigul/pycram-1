9b3ac53fafc6ec22f42d3085294ae14bdf8febf6
Jonas Dech <j.dech@gmx.net>
2021-10-14 18:09:30 +0200

    integrated new sparql query

--- src/pycram/knowrob.py
+++ src/pycram/knowrob.py
@@ -2,39 +2,16 @@ from json_prolog_msgs.srv import PrologQuery, PrologNextSolution, PrologFinish
 from knowrob_refills.knowrob_wrapper import KnowRob
 from rosprolog_client import Prolog, PrologException
 import rospy
+import numpy as np
 
 prolog = Prolog()
 
-def startup_knowrob():
-    db = '~/workspace/whole_store_filled_neem/roslog'
-    knowrob = KnowRob(initial_mongo_db=db,
-                      clear_roslog=True,
-                      republish_tf=True,
-                      neem_mode=False)
-    # knowrob = KnowRob(initial_mongo_db=None,
-    #                   clear_roslog=False)
-    shelf_ids = knowrob.get_shelf_system_ids(False)
-    print(shelf_ids)
-    for shelf_id in shelf_ids:
-        print('shelf center frame id {}'.format(knowrob.get_object_frame_id(shelf_id)))
-        print('shelf corner frame id {}'.format(knowrob.get_perceived_frame_id(shelf_id)))
-    return knowrob
-
-def get_all_shelves():
-    #prolog = Prolog()
-    return prolog.once("get_all_shelves(A)")['A']
-
-def find_shelf_pose(shelf):
-    #prolog = Prolog()
-    pose = prolog.once(f"get_pose_in_desired_reference_frame('{shelf}', 'map', T, R)")
-    return [pose['T'], pose['R']]
-
 def get_pose_for_product_type(product_type):
-
-    query = prolog.once(f"subclass_of(Product, '{product_type}'), has_type(Item, Product).")
     prolog.once("use_module(library('semweb/sparql_client')).")
+    # query = prolog.once(f"subclass_of(Product, '{product_type}'), has_type(Item, Product).")
+    # prolog.once("use_module(library('semweb/sparql_client')).")
     #?product tax:has_EAN ?EAN.
-    sparq = " sparql_query('PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> \
+    sparq = "sparql_query('PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> \
     PREFIX loc: <http://knowrob.org/kg/ProductToShelf.owl#> \
     PREFIX owl: <http://www.w3.org/2002/07/owl#> \
     PREFIX tax: <http://knowrob.org/kg/ProductTaxonomy.owl#> \
@@ -44,14 +21,48 @@ def get_pose_for_product_type(product_type):
         ?prod loc:stored_in ?shelf.\
     } ', Row,\
     [ endpoint('https://api.krr.triply.cc/datasets/mkumpel/TrustNonFoodKG/services/TrustNonFoodKG/sparql/')])."
-    #sparq = sparq.replace('TYPE', product_type)
-    #product_type = prolog.once(sparq)
-    #print(product_type)
-    #shelf_pose = prolog
-    #query = prolog.once(f"instance_of(Product, '{product_type}'), subclass_of(Product, Class).")
-    query = prolog.once(f"get_product_location('{product_type}', Item, Shelf, Layer, Facing)")
-    #print(query)
-    item = query['Item']
-    item_pose = prolog.once(f"get_pose_in_desired_reference_frame('{item}', 'map', T, R)")
-    print(item_pose)
-    return [item_pose['T'], item_pose['R']]
+    sparq = sparq.replace('TYPE', product_type)
+    product_query = prolog.once(sparq)
+    print(product_query)
+    if product_query == []:
+        raise PrologException(f"{product_type} is not in the product taxonomy")
+
+    # The shelf returned by the taxonomy, this is not the same as the one in the belief state
+    shelf_sparq = product_query['Row']['term'][1]
+    shelf_number = int(shelf_sparq.split('Shelf')[2])
+    # All shelves in the belief state
+    shelves = prolog.once("get_all_shelves(S)")['S']
+    for shelf in shelves:
+        # The shelf id tag and corresponding marker. The id tag is identical to the number
+        # returned by the taxonomy
+        tag = prolog.once(f"shelf_with_marker('{shelf}', Marker), holds(Marker, dmshop:markerId, ID).")
+        id = int(tag['ID'].split('_')[1])
+        id_shelf = np.ceil(id/2)
+        if shelf_number == id_shelf:
+            pose = prolog.once(f"is_at('{tag['Marker']}', ['map', T, R])")
+            return pose['T'], pose['R']
+    raise PrologException("No shelf position was found for the shelf the product is stored in.")
+
+
+def _generate_query(args):
+    query = "sparql_query('PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> \
+    PREFIX loc: <http://knowrob.org/kg/ProductToShelf.owl#> \
+    ADD_PREFIX \
+    PREFIX owl: <http://www.w3.org/2002/07/owl#> \
+    PREFIX tax: <http://knowrob.org/kg/ProductTaxonomy.owl#> \
+    select ?shelf where{ \
+        ADD_QUERY \
+        FILTER \
+        ?product rdf:type tax:TYPE. \
+        ?product owl:sameAs ?prod.\
+        ?prod loc:stored_in ?shelf.\
+    } ', Row,\
+    [ endpoint('https://api.krr.triply.cc/datasets/mkumpel/TrustNonFoodKG/services/TrustNonFoodKG/sparql/')])."
+
+    ingredient_query = "?prod in:has_ingredient ?ingredient. \
+                        ?ingredient rdf:type ?inclass."
+    ingredient_filter = "FILTER(?inclass = in:ING)"
+    ingredient_prefix = "PREFIX in: <http://knowrob.org/kg/ingredients.owl#>"
+
+    query = query.replace("TYPE", "tooth_paste").replace("ADD_QUERY", ingredient_query).replace("FILTER", ingredient_filter.replace("ING", "Plant_extract")).replace("ADD_PREFIX", ingredient_prefix)
+    return query
