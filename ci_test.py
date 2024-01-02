107779cddadddd359e100ad0a7a166c980b3c637
Jonas Dech <jdech@uni-bremen.de>
2024-01-02 22:59:12 +0100

    without moveTCP

--- ci_test.py
+++ ci_test.py
@@ -36,7 +36,7 @@ with simulated_robot:
     with Use_shadow_world():
         shadow_robot.set_pose(Pose([1,2,0]))
         shadow_robot.set_pose(Pose([0,0,0]))
-        MoveTCPMotion(Pose([0.5, -0.7, 1])).resolve().perform()
+        #MoveTCPMotion(Pose([0.5, -0.7, 1])).resolve().perform()
 print("test blocking")
 print("end blocking test")
 
