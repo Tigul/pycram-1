fdccea10d856b5fe7b0daffbcbea98f1f51a0f6b
Jonas Dech <jdech@uni-bremen.de>
2024-01-02 19:59:51 +0100

    removed milk

--- ci_test.py
+++ ci_test.py
@@ -7,17 +7,17 @@ from pycram.bullet_world_reasoning import *
 
 world = BulletWorld("DIRECT")
 robot = Object("pr2", ObjectType.ROBOT, "pr2.urdf")
-milk = Object("milk", ObjectType.MILK, "milk.stl", pose=Pose([1.3, 1, 0.9]))
+#milk = Object("milk", ObjectType.MILK, "milk.stl", pose=Pose([1.3, 1, 0.9]))
 #apartement = Object("apartement", ObjectType.ENVIRONMENT, "apartment.urdf")
 
 print("start blocking test")
-milk.set_pose(Pose([0.5, -0.7, 1]))
+#milk.set_pose(Pose([0.5, -0.7, 1]))
 print("move milk")
 robot.set_pose(Pose())
 print("move robot")
 time.sleep(1)
 print("sleep")
-blocking(Pose([0.5, -0.7, 1]), robot, robot_description.get_tool_frame("right"))
+#blocking(Pose([0.5, -0.7, 1]), robot, robot_description.get_tool_frame("right"))
 print("test blocking")
 print("end blocking test")
 
