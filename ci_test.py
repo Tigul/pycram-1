45d9fa3f5c42e14a8c3005a48b10b46beaadfaf8
Jonas Dech <jdech@uni-bremen.de>
2024-01-02 23:30:18 +0100

    print service list

--- ci_test.py
+++ ci_test.py
@@ -11,6 +11,7 @@ from pycram.designators.object_designator import *
 from pycram.process_module import simulated_robot
 from pycram.bullet_world_reasoning import *
 from pycram.external_interfaces.ik import request_ik
+import rosservice
 
 print("before world")
 rospy.logwarn("before world")
@@ -37,8 +38,8 @@ with simulated_robot:
     with Use_shadow_world():
         shadow_robot.set_pose(Pose([1,2,0]))
         shadow_robot.set_pose(Pose([0,0,0]))
-
-        rospy.wait_for_service("/pr2_right_arm_kinematics/get_ik")
+        print(rosservice.get_service_list())
+        #rospy.wait_for_service("/pr2_right_arm_kinematics/get_ik")
 
         #request_ik(Pose([0.5, -0.7, 1]), robot, robot_description.chains["right"].joints, robot_description.get_tool_frame("right"))
         #MoveTCPMotion(Pose([0.5, -0.7, 1])).resolve().perform()
