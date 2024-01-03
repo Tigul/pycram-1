8fe753141094f5b784f4dca221a423546e803028
Jonas Dech <jdech@uni-bremen.de>
2024-01-03 12:50:51 +0100

    test commit

--- test/test_bullet_world.py
+++ test/test_bullet_world.py
@@ -32,6 +32,7 @@ class BulletWorldTest(unittest.TestCase):
     #    self.milk.set_position(Pose([0, 1, 1]))
     #    self.assertEqual(self.milk.get_pose().position_as_list(), [0, 1, 1])
 
+
     #def test_robot_orientation(self):
     #    self.robot.set_pose(Pose([0, 1, 1]))
     #    head_position = self.robot.get_link_position('head_pan_link').z
